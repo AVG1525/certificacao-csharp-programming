bc56a13b2018c80ad22feec8c228c2fd3b104128
Arrow <aronvinicius15@gmail.com>
2020-10-06 11:34:07 -0300

    adicionando estrutura do 1 modulo e praticas feitas na aula de tipos de valor

---
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/antes/TiposDeValor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 30;
+
+            Console.WriteLine(idade);
+
+            int copia_idade = idade;
+
+            Console.WriteLine($"idade: {idade} - copia idade {copia_idade}");
+
+            idade = 23;
+
+            Console.WriteLine($"idade: {idade} - copia idade {copia_idade}");
+
+            int? idade2 = null;
+            System.Nullable<int> idade3 = null;
+        }
+    }
+}
---
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/antes/Inteiros.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class TiposInteiros : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 15;
+            //idade = 15.5;
+
+            char resposta = 'S'; // System.Char
+
+            byte nivel_de_azul = 0xFF; // = 255 em decimal
+
+            short passageiros_voo = 230; // System.Int16
+            passageiros_voo = -7;
+
+            int populacao = 1500; // System.Int32
+            populacao = -2300;
+
+            long populacao_do_brasil = 207_660_929; // +/- 207 milhoes - System.Int64
+
+            sbyte nivel_de_brilho = -127; // System.Sbyte -> permite "byte" com numero negativos
+
+            ushort passageiros_navio = 230; //System.UInt16 -> unsigned, ele não armazena o bit do sinal (+ ou -)
+
+            uint estoque = 1500; // System.UInt32 -> unsigned, ele não armazena o bit do sinal
+
+            ulong populacao_do_mundo = 7_000_000_000; // 7 bilhoes - System.UInt64
+        }
+    }
+}
