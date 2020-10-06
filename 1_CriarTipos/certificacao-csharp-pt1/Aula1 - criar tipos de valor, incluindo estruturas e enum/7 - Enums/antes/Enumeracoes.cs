2176ddd16258a28f8d85984d5090f82977ba60d3
Arrow <aronvinicius15@gmail.com>
2020-10-06 15:59:25 -0300

    finalizando praticas dos tipos enums

--- 1_CriarTipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/7 - Enums/antes/Enumeracoes.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/7 - Enums/antes/Enumeracoes.cs
@@ -10,6 +10,36 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            DiasDaSemana primeiro_dia = DiasDaSemana.Seg;
+            DiasDeTrabalho dias_de_trabalho = 
+                DiasDeTrabalho.Ter | DiasDeTrabalho.Qui | DiasDeTrabalho.Sex;
+
+            Console.WriteLine(dias_de_trabalho);
         }
+    }
+
+    enum DiasDaSemana : long
+    { 
+        Seg = 3,
+        Ter,
+        Qua,
+        Qui,
+        Sex,
+        Sab,
+        Dom
+    }
+
+    // se não colocar o flags ele vai me dar os valores de cada um como por exeplo Seg é 0. 
+    // Quando tem o atributo ele me retorna o nome do valor selecionado, por exemplo se selecionei 0 então retorna Seg.
+    [Flags] 
+    enum DiasDeTrabalho
+    {
+        Seg = 0,
+        Ter = 1,
+        Qua = 2,
+        Qui = 4,
+        Sex = 8,
+        Sab = 16,
+        Dom = 32
     }
 }
