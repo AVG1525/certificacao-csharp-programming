8b8475156e4075e0522d206068dedf33d467857b
Arrow <aronvinicius15@gmail.com>
2020-10-06 15:48:29 -0300

    finalizando praticas dos tipos structs

--- 1_CriarTipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/antes/Estruturas.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/antes/Estruturas.cs
@@ -18,6 +18,41 @@ namespace certificacao_csharp_roteiro.antes
             Console.WriteLine($"Longitude1 = {Longitude1}");
             Console.WriteLine($"Latitude2 = {Latitude2}");
             Console.WriteLine($"Longitude2 = {Longitude2}");
+
+            PosicaoGPS posicao1;
+            posicao1.Latitude = 13.78;
+            posicao1.Longitude = 29.51;
+
+            posicao1 = new PosicaoGPS(13.78, 29.51);
+            Console.WriteLine();
+            Console.WriteLine(posicao1);
+        }
+    }
+
+    interface IGPS
+    {
+        bool EstaNoHemisferioNorte();
+    }
+
+    struct PosicaoGPS : IGPS //struct não suporte herança, só a interface mesmo
+    { // structs são tipo de valor pq quando copiadas pra outra variavel as duas podem tomar caminhos diferentes... ja uma classe é por referencia
+        public double Latitude;
+        public double Longitude;
+
+        public PosicaoGPS(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public bool EstaNoHemisferioNorte()
+        {
+            return Latitude > 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude} e Longitude: {Longitude}";
         }
     }
 }
