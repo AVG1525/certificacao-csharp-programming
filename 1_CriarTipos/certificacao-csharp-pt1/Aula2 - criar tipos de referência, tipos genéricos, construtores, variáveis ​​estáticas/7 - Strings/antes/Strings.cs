5d28718402e2628a6685aa2db1d8a32796b60510
Arrow <aronvinicius15@gmail.com>
2020-10-07 18:22:05 -0300

    finalizando praticas das strings

--- 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/7 - Strings/antes/Strings.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/7 - Strings/antes/Strings.cs
@@ -10,8 +10,22 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
-            string a = "bom dia";
+            string a = "bom dia"; //System.String -> é um tipo de referencia
             string b = "b";
+
+            b += "om dia";
+            Console.WriteLine($"a == b: {a == b}");
+            Console.WriteLine();
+            // Verificando se contendo o mesmo texto as duas variaveis string apontam pro mesmo endereco na memoria
+            Console.WriteLine($"(object)a == (object)b: {(object)a == (object)b}");
+
+            string bomDia = "Bom dia";
+            Console.WriteLine();
+            Console.WriteLine($"bomDia[4]: {bomDia[4]}");
+
+            var caractere = bomDia[4];
+            Console.WriteLine();
+            Console.WriteLine($"caractere.GetType(): {caractere.GetType()}"); // Resultado -> System.Char
         }
     }
 }
