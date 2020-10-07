5f28572ef80d3a42034043cfdf6d88b0317c1ff9
Arrow <aronvinicius15@gmail.com>
2020-10-07 17:32:25 -0300

    finalizando praticas dos object

--- 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/5 - Objetos/antes/Objetos.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/5 - Objetos/antes/Objetos.cs
@@ -10,8 +10,24 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
-           int pontuacao = 10;
-           Console.WriteLine($"pontuacao: {pontuacao}");
+            int pontuacao = 10;
+            Console.WriteLine($"pontuacao: {pontuacao}");
+
+            Console.WriteLine();
+            Console.WriteLine("OBJECT COM VALOR PRIMITOVO");
+            object meuObjeto;
+            meuObjeto = pontuacao;
+            Console.WriteLine($"Meu objeto: {meuObjeto}");
+            Console.WriteLine($"MeuObjeto.GetType(): {meuObjeto.GetType()}");
+            
+            Console.WriteLine();
+            Console.WriteLine("OBJECT COM REFERENCIA DE OBJETO");
+
+            meuObjeto = new Jogador();
+            Jogador classRef;
+            classRef = (Jogador)meuObjeto; //conevrsão explícita ou "cast"
+
+            Console.WriteLine($"classRef.Pontuacao: {classRef.Pontuacao}");
         }
     }
 
