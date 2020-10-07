f5195ce190799a6aad480aba0e894de172e319b6
Arrow <aronvinicius15@gmail.com>
2020-10-07 17:19:32 -0300

    finalizando praticas dos delegades

--- 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/4 - Delegates/antes/Delegates.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/4 - Delegates/antes/Delegates.cs
@@ -14,6 +14,8 @@ namespace certificacao_csharp_roteiro.antes
         }
     }
 
+    delegate double MetodoMultiplcacao(double input);
+
     class Calculadora
     {
         static double Duplicar(double input)
@@ -30,10 +32,37 @@ namespace certificacao_csharp_roteiro.antes
         {
 
             //Executa diretamente o método
+            Console.WriteLine();
             Console.WriteLine($"Duplicar(7.5): {Duplicar(7.5)}");
 
             //Executa diretamente o método
+            Console.WriteLine();
             Console.WriteLine($"Triplicar(7.5): {Triplicar(7.5)}");
+
+            MetodoMultiplcacao metodoMultiplcacao = Duplicar;
+            Console.WriteLine();
+            Console.WriteLine($"Duplicar: {metodoMultiplcacao(7.5)}");
+
+            metodoMultiplcacao = Triplicar;
+            Console.WriteLine();
+            Console.WriteLine($"Triplicar: {metodoMultiplcacao(7.5)}");
+
+            //instancia de um delegado com um metodo anonimo
+            MetodoMultiplcacao metodoQuadrado = delegate (double input)
+            {
+                return input * input;
+            };
+
+            double quadrado = metodoQuadrado(5);
+            Console.WriteLine();
+            Console.WriteLine($"Quadrado: {quadrado}");
+
+
+            //instancia de um delegado com uma expressao lambda
+            MetodoMultiplcacao metodoCubo = input => input * input * input;
+            double cubo = metodoCubo(4.375);
+            Console.WriteLine();
+            Console.WriteLine($"Cubo: {cubo}");
         }
     }
 }
