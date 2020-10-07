18b37d7f8aef7c87782b77bb5a6330b04805b436
Arrow <aronvinicius15@gmail.com>
2020-10-07 15:38:31 -0300

    finalizando praticas das classes

--- 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/antes/Classes.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/antes/Classes.cs
@@ -10,6 +10,57 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            ClassePosicaoGPS posicao1 = new ClassePosicaoGPS() 
+            { Latitude = 13.78, Longitude = 29.51 };
+
+            posicao1 = new ClassePosicaoGPS(13.78, 29.51);
+            Console.WriteLine();
+            Console.WriteLine(posicao1);
+
+            PosicaoGPSComLeitura posicao2 = new PosicaoGPSComLeitura(13.78, 29.51, DateTime.Now);
+            Console.WriteLine();
+            Console.WriteLine(posicao2);
+        }
+    }
+
+    public class ClassePosicaoGPS : IGPS
+    {
+        public double Latitude;
+        public double Longitude;
+
+        public ClassePosicaoGPS()
+        {
+
+        }
+        public ClassePosicaoGPS(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public bool EstaNoHemisferioNorte()
+        {
+            return Latitude > 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude} e Longitude: {Longitude}";
+        }
+    }
+
+    public class PosicaoGPSComLeitura : ClassePosicaoGPS
+    {
+        private readonly DateTime DataLeitura;
+
+        public PosicaoGPSComLeitura(double latitude, double longitude, DateTime dataLeitura) : base(latitude, longitude)
+        {
+            DataLeitura = dataLeitura;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude}, Longitude: {Longitude} e DataLeitura: {DataLeitura}";
         }
     }
 }
