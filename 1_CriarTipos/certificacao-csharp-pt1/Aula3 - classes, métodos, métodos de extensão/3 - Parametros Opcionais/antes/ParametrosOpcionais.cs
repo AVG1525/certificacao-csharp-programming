7ea3187c1e11b3c65de04767eef70fafc4acfcaa
Arrow <aronvinicius15@gmail.com>
2020-10-13 12:07:06 -0300

    Finalizando atividades praticas sobre aula de parametros opcionais

--- 1_CriarTipos/certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/antes/ParametrosOpcionais.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/antes/ParametrosOpcionais.cs
@@ -10,19 +10,30 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            ClienteEspecial clienteEspecial = new ClienteEspecial("Lucas");
+            clienteEspecial.FazerPedido(1, "Residencial", 1);
 
+            clienteEspecial = new ClienteEspecial();
+            clienteEspecial.FazerPedido(1, "Residencial", 1);
+
+            clienteEspecial.FazerPedido(2, "Comercial");
+
+            clienteEspecial.FazerPedido(3);
+
+            clienteEspecial.FazerPedido(3, quantidade: 4);
         }
     }
 
     class ClienteEspecial
     {
         private readonly string nome;
-        public ClienteEspecial(string nome)
+        public ClienteEspecial(string nome = "Matheus")
         {
             this.nome = nome;
         }
 
-        public void FazerPedido(int produtoId, string endereco, int quantidade)
+        //Os parametros opcionais devem aparecer por último para não darem erro de compilação
+        public void FazerPedido(int produtoId, string endereco = "Residencial", int quantidade = 10)
         {
             Console.WriteLine("cliente {0}: produtoId: {1}, endereço: {2}, quantidade: {3}.", nome, produtoId, endereco, quantidade);
         }
