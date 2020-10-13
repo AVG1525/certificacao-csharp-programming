93561584b9cf9f88a1cfbb27b042509716a7b092
Arrow <aronvinicius15@gmail.com>
2020-10-13 10:14:41 -0300

    Finalizando atividades praticas sobre aula de metodos, modificadores de acesso, etc

--- 1_CriarTipos/certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/2 - Metodos/antes/Metodos.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/2 - Metodos/antes/Metodos.cs
@@ -11,13 +11,27 @@ namespace certificacao_csharp_roteiro.antes
         public void Executar()
         {
             Retangulo retangulo = new Retangulo(12, 10);
+
+            Console.WriteLine($"area: {retangulo.GetArea()}");
+
+            Retangulo outroRetangulo = new Retangulo(10, 10);
+            Console.WriteLine($"");
+            Console.WriteLine($"retangulo semelhante: {retangulo.Semelhante(outroRetangulo.Altura, outroRetangulo.Largura)}");
+
+            outroRetangulo = new Retangulo(5, 6);
+            Console.WriteLine($"");
+            Console.WriteLine($"retangulo semelhante 2 : {retangulo.Semelhante(outroRetangulo)}");
+
+            outroRetangulo = new Retangulo(5, 6);
+            Console.WriteLine($"");
+            Console.WriteLine($"retangulo semelhante 3 : {Retangulo.Semelhante(retangulo, outroRetangulo)}");
         }
     }
 
     class Retangulo
     {
-        public double Altura { get; set; }
-        public double Largura { get; set; }
+        public double Altura { get; set; } // Restringir a faixa de valores que podem ser atribuidos ao membro == { set; }
+        public double Largura { get; set; } // { set; } (regras no acessador set para validar e restringir a faixa de valores aceitos)
 
         public Retangulo(double altura, double largura)
         {
@@ -27,12 +41,14 @@ namespace certificacao_csharp_roteiro.antes
             Console.WriteLine($"altura: {altura}, largura: {largura}");
         }
 
+        // public, internal, protrect (+internal), private (+internal)
+
         public double GetArea()
         {
             return Altura * Largura;
         }
 
-        bool Semelhante(double outroRetanguloAltura, double outroRetanguloLargura)
+        internal bool Semelhante(double outroRetanguloAltura, double outroRetanguloLargura)
         {
             return
                 ((Largura / Altura) == /*proporção deste retângulo*/
@@ -40,6 +56,26 @@ namespace certificacao_csharp_roteiro.antes
                 ||
                 ((Altura / Largura) == /*compara a proporção inversa*/
                 (outroRetanguloLargura / outroRetanguloAltura));
+        }
+
+        internal bool Semelhante(Retangulo outroRetangulo)
+        {
+            return
+                ((Largura / Altura) == /*proporção deste retângulo*/
+                (outroRetangulo.Largura / outroRetangulo.Altura)) /*proporção do outro retângulo*/
+                ||
+                ((Altura / Largura) == /*compara a proporção inversa*/
+                (outroRetangulo.Largura / outroRetangulo.Altura));
+        }
+
+        internal static bool Semelhante(Retangulo retangulo, Retangulo outroRetangulo)
+        {
+            return
+                ((retangulo.Largura / retangulo.Altura) == /*proporção deste retângulo*/
+                (outroRetangulo.Largura / outroRetangulo.Altura)) /*proporção do outro retângulo*/
+                ||
+                ((retangulo.Altura / retangulo.Largura) == /*compara a proporção inversa*/
+                (outroRetangulo.Largura / outroRetangulo.Altura));
         }
     }
 }
