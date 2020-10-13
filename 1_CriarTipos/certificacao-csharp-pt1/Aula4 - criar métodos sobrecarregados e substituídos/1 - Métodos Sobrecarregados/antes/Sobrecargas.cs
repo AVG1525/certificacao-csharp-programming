b4a99924abf75d10c12af4fbb9d954c19ba3d933
Arrow <aronvinicius15@gmail.com>
2020-10-13 13:57:30 -0300

    Finalizando atividades praticas sobre aula de metodos sobrecarregados

--- 1_CriarTipos/certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/1 - Métodos Sobrecarregados/antes/Sobrecargas.cs
+++ 1_CriarTipos/certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/1 - Métodos Sobrecarregados/antes/Sobrecargas.cs
@@ -13,14 +13,41 @@ namespace certificacao_csharp_roteiro.antes
             //VOLUME DO CUBO = lado ^ 3;
             int lado = 3;
 
+            Console.WriteLine();
+            //Console.WriteLine($"Volume do cubo: {volumeCubo(lado)}");
+            Console.WriteLine($"Volume do cubo: {volume(lado)}");
+
             //VOLUME DO CILINDRO = altura * PI * raio ^ 2;
             double raio = 2;
             int altura = 10;
+
+            Console.WriteLine();
+            //Console.WriteLine($"Volume do cilindro: {volumeCilindro(altura, raio)}");
+            Console.WriteLine($"Volume do cubo: {volume(altura, raio)}");
 
             //VOLUME DO PRISMA = largura * profundidade * altura
             long largura = 10;
             altura = 6;
             int profundidade = 4;
+
+            Console.WriteLine();
+            //Console.WriteLine($"Volume do prisma: {volumePrisma(largura, altura, profundidade)}");
+            Console.WriteLine($"Volume do prisma: {volume(largura, altura, profundidade)}");
+        }
+
+        public double volume(double lado)
+        {
+            return Math.Pow(lado, 3);
+        }
+
+        public double volume(double altura, double raio)
+        {
+            return altura * Math.PI * Math.Pow(raio, 2);
+        }
+
+        public double volume(double largura, double profundidade, double altura)
+        {
+            return largura * profundidade * altura;
         }
     }
 }
