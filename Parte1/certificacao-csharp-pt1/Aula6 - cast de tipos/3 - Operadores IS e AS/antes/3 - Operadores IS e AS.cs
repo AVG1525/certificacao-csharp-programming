f089600b8fae1b6ed5ff7a0c28c6f17d41ea888f
Arrow <aronvinicius15@gmail.com>
2021-01-16 11:01:09 -0300

    estudo sobre operadores is que realiza a verificacao dos tipos e retorna um booleano e as que realiza a verificacao dos tipos e retorna um null caso der false e o cast caso der true

--- Parte1/certificacao-csharp-pt1/Aula6 - cast de tipos/3 - Operadores IS e AS/antes/3 - Operadores IS e AS.cs
+++ Parte1/certificacao-csharp-pt1/Aula6 - cast de tipos/3 - Operadores IS e AS/antes/3 - Operadores IS e AS.cs
@@ -13,13 +13,37 @@ namespace certificacao_csharp_roteiro.antes
             Animal animal = new Animal();
             Gato gato = new Gato();
             Cliente cliente = new Cliente("José da Silva", 30);
+
+            Alimentar(animal);
+            Alimentar(gato);
+            Alimentar(cliente);
         }
 
         public void Alimentar(object obj)
         {
-            Animal animal = (Animal)obj;
+            //Animal animal = null;
+
+            //if (obj is Animal) animal = (Animal)obj; // ou  (obj is IAnimal)
+
+            /*Animal animal = obj as Animal;
+
+            if (animal == null)
+            {
+                Console.WriteLine("obj não é um animal.");
+                return;
+            }
+
             animal.Beber();
-            animal.Comer();
+            animal.Comer();*/
+
+            if(obj is Animal animal)
+            {
+                animal.Beber();
+                animal.Comer();
+                return;
+            }
+
+            Console.WriteLine("obj não é um animal.");
         }
     }
 }
