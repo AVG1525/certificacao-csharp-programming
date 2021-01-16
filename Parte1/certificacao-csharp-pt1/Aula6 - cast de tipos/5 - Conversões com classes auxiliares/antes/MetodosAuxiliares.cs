90b41261be8cf3a4608234ed2d1f9d3cc0e3dbb4
Arrow <aronvinicius15@gmail.com>
2021-01-16 12:48:09 -0300

    estudos sobre conversoes utilizando o try parse

--- Parte1/certificacao-csharp-pt1/Aula6 - cast de tipos/5 - Conversões com classes auxiliares/antes/MetodosAuxiliares.cs
+++ Parte1/certificacao-csharp-pt1/Aula6 - cast de tipos/5 - Conversões com classes auxiliares/antes/MetodosAuxiliares.cs
@@ -11,6 +11,26 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            string textoDigitado = "123";
+            //int numeroConvertido = textoDigitado;
+
+            int numeroConvertido = int.Parse(textoDigitado);
+            Console.WriteLine(numeroConvertido);
+
+            textoDigitado = "abc";
+            //numeroConvertido = int.Parse(textoDigitado);
+            //int.TryParse(textoDigitado, out numeroConvertido);
+            if(int.TryParse(textoDigitado, out numeroConvertido)) Console.WriteLine(numeroConvertido);
+            else Console.WriteLine("Texto não é um número.");
+
+            textoDigitado = "R$ 123,45";
+            decimal.TryParse(
+                textoDigitado,
+                NumberStyles.Currency, //moeda
+                CultureInfo.CurrentCulture, //cultura atual => pt-br
+                out decimal valorDigitado
+            );
+            Console.WriteLine(valorDigitado);
         }
     }
 }
