0c6421c5235508ef89ddb65714003d62d50a0013
Arrow <aronvinicius15@gmail.com>
2021-01-16 13:34:38 -0300

    estudo sobre as diferenças do string, var, object e dynamic

--- Parte1/certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/1 - string, var, object e dynamic/antes/UsandoDynamic.cs
+++ Parte1/certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/1 - string, var, object e dynamic/antes/UsandoDynamic.cs
@@ -10,6 +10,43 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            // tipo é definido em tempo de compilação
+            string s = "Certificação c#";
+            // tipo é definido em tempo de compilação (por inferencia)
+            var v = "Certificação c#";
+
+            // tipo é definido em tempo de compilação
+            object o = "Certificação c#";
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+            s = s.ToUpper();
+            v = v.ToUpper();
+            o = ((string)o).ToUpper();
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+
+            o = 123;
+            o = (int)o + 4;
+            Console.WriteLine(o);
+
+            // tempo é definido em tempo de execução
+            dynamic d = "Certificação c#";
+            Console.WriteLine(d);
+
+            d = d.ToUpper();
+            Console.WriteLine(d);
+
+            d = 123;
+            Console.WriteLine(d);
+
+            d = d + 4;
+            Console.WriteLine(d);
         }
     }
 }
