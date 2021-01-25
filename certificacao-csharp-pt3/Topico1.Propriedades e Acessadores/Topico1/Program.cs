b504bb2a44a909b35be659f3d11bfda99d26324b
Arrow <aronvinicius15@gmail.com>
2021-01-25 15:00:57 -0300

    adicionando console application para parte 3 e estudo sobre getters e setters

---
+++ certificacao-csharp-pt3/Topico1.Propriedades e Acessadores/Topico1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Topico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            //funcionario.salario = 1000;
+
+            //Console.WriteLine(funcionario.salario);
+
+
+            //funcionario.salario = -1200;
+            //Console.WriteLine(funcionario.salario);
+
+            funcionario.Salario = 1200;
+            Console.WriteLine(funcionario.Salario);
+        }
+    }
+
+    class Funcionario
+    {
+        decimal salario; //se nao definir ele cria implicitamente como private
+
+        public decimal Salario // encapsulamento do campo salário
+        {
+            get
+            {
+                return salario;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("salario nao pode ser negativo");
+                salario = value;
+            }
+        }
+    }
+}
