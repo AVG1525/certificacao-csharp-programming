46edea454fd269514c998a50be45ba7764fa898c
Arrow <aronvinicius15@gmail.com>
2021-02-05 09:38:48 -0300

    add prof topico 5

---
+++ certificacao-csharp-pt3/Topico1.Propriedades e Acessadores/Topico5.ComparacoesMod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Topico5.ComparacoesMod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
