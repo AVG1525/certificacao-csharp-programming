156699a25400e3d40c15a68023c91d4b97000fd1
Arrow <aronvinicius15@gmail.com>
2021-02-09 11:48:58 -0300

    aula sobre equals

--- certificacao-csharp-pt3/Topico1.Propriedades e Acessadores/Topico5.ComparacoesMod/Program.cs
+++ certificacao-csharp-pt3/Topico1.Propriedades e Acessadores/Topico5.ComparacoesMod/Program.cs
@@ -6,7 +6,53 @@ namespace Topico5.ComparacoesMod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Aluno aluno1 = new Aluno
+            {
+                Nome = "José da Silva",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
+
+            Aluno aluno2 = new Aluno
+            {
+                Nome = "José da Silva",
+                DataNascimento = new DateTime(1995, 1, 1)
+            };
+
+            Aluno aluno3 = new Aluno
+            {
+                Nome = "josé da silva",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
+
+            Console.WriteLine(aluno1.Equals(aluno2));
+            Console.WriteLine(aluno1.Equals(aluno3)) ;
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Data nascimento: {DataNascimento:dd/MM/yyyy}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Aluno outroAluno = obj as Aluno;
+
+            if (obj == null) return false;
+
+            return 
+                Nome.Equals(outroAluno.Nome, StringComparison.CurrentCultureIgnoreCase) &&
+                DataNascimento.Equals(outroAluno.DataNascimento);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Nome, DataNascimento);
         }
     }
 }
