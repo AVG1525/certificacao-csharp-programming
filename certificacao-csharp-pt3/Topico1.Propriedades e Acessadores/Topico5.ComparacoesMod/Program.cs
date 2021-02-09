6a21930670268b19d6cd2585b84b84c4cb3291cb
Arrow <aronvinicius15@gmail.com>
2021-02-09 12:28:58 -0300

    comparacoes

--- certificacao-csharp-pt3/Topico1.Propriedades e Acessadores/Topico5.ComparacoesMod/Program.cs
+++ certificacao-csharp-pt3/Topico1.Propriedades e Acessadores/Topico5.ComparacoesMod/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Topico5.ComparacoesMod
 {
@@ -25,11 +26,39 @@ namespace Topico5.ComparacoesMod
             };
 
             Console.WriteLine(aluno1.Equals(aluno2));
-            Console.WriteLine(aluno1.Equals(aluno3)) ;
+            Console.WriteLine(aluno1.Equals(aluno3));
+
+            Aluno aluno4 = new Aluno
+            {
+                Nome = "ANDRÉ DOS SANTOS",
+                DataNascimento = new DateTime(1970, 1, 1)
+            };
+
+            Aluno aluno5 = new Aluno
+            {
+                Nome = "Ana de Souza",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
+
+            List<Aluno> alunos = new List<Aluno>
+            {
+                aluno1,
+                aluno2,
+                aluno3,
+                aluno4,
+                aluno5
+            };
+
+            alunos.Sort();
+
+            foreach(var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
         }
     }
 
-    class Aluno
+    class Aluno : IComparable
     {
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
@@ -53,6 +82,24 @@ namespace Topico5.ComparacoesMod
         public override int GetHashCode()
         {
             return HashCode.Combine(Nome, DataNascimento);
+        }
+
+        public int CompareTo(object obj)
+        {
+            // retornar 0 => objetos são iguais
+            // retornar > 0 => objeto atual vem depois
+            // retornar < 0 => objeto atual vem antes
+
+            if (obj == null) return 1;
+
+            Aluno outroAluno = obj as Aluno;
+            if (outroAluno == null) throw new ArgumentException("Objeto não pode ser convertido");
+
+            int ret = DataNascimento.CompareTo(outroAluno.DataNascimento);
+
+            if (ret == 0) ret = Nome.CompareTo(outroAluno.Nome);
+
+            return ret;
         }
     }
 }
